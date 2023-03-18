323122580b1f94c13ed19f03fe4bb8913a0b2593
Nike159 <108959165+Nike159@users.noreply.github.com>
2023-03-18 00:36:39 -0500

    Laying out page

--- midwesthire/src/Components/Landing.jsx
+++ midwesthire/src/Components/Landing.jsx
@@ -7,11 +7,17 @@ import About from '../Assets/About.jpg';
 function Landing() {
   return (
     <div>
-      <Parallax strength={600} bgImage={LandingImg} className="LandingImg">
+      <Parallax strength={600} bgImage={LandingImg} >
         <div className='content'>
           <div className='text-content'>Midwest Hire</div>
         </div>
       </Parallax>
+      <h1 className='About'>Who We Are</h1>
+      <Parallax strength={-600} bgImage={LandingImg} >
+        <div className='AboutContainer'>
+
+        </div>
+    </Parallax>
 <div className='content'></div>
     </div>
   )
